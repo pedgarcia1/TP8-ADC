b4ad8bc85f607a643a2701bf461a4c0c36851729
pedgarcia1 <pedgarcia@itba.edu.ar>
2023-06-24 18:16:43 -0300

    1ra version de timer y ADC

--- ADC.c
+++ ADC.c
@@ -1,4 +1,4 @@
-#include <msp430.h> 
+#include "msp430g2553.h" 
 
 void adcinit(void);
 
@@ -31,12 +31,12 @@ int main(void)
     int i;
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
-	adcinit();
+	adcInit();
 
 	while(1)
 	{
 	    adcval=ADC10MEM; // Read adc value
-	    ADC10CTL0 |= ENC + ADC10SC; // Enable converter & Start of conversión  (insert a breakpoint here and watch variable adcval)
+	    ADC10CTL0 |= ENC + ADC10SC; // Enable converter & Start of conversiï¿½n  (insert a breakpoint here and watch variable adcval)
 	    for(i=20000; i>0; i--);          // some delay
 
 	}
@@ -49,11 +49,11 @@ int main(void)
 
 
 
-void adcinit(void)
+void adcInit(void)
 {
     /* Select Reference */
     ADC10CTL0 = SREF_0 + ADC10SHT_2 + ADC10ON;
-  //  ADC10CTL0 = SREF_1 + ADC10SHT_2 + ADC10ON + REF2_5V + REFON;
+  	//  ADC10CTL0 = SREF_1 + ADC10SHT_2 + ADC10ON + REF2_5V + REFON;
 
     /*Select input channel P1.7*/
     ADC10CTL1 = INCH_7; // input A7
@@ -61,4 +61,14 @@ void adcinit(void)
 
     /* Enable  P1.7 pin as Analog innput (A7) */
     ADC10AE0 |= 0x80; // P1.7 ADC option select
+}
+
+unsigned int readADC(void)
+{
+	unsigned int adcval;
+	ADC10CTL0 |= ENC + ADC10SC; // Enable converter & Start of conversion
+	// chequear esta parte de busy
+	while (ADC10CTL1 & BUSY); // Wait if ADC10 core is active
+	adcval=ADC10MEM; // Read adc value
+	return adcval;
 }
