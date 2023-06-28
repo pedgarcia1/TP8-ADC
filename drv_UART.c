640a711e9ecf5397da88d6d99a6d96d1be7d5864
pedgarcia1 <pedgarcia@itba.edu.ar>
2023-06-28 20:48:18 -0300

    algo anda

--- drv_UART.c
+++ drv_UART.c
@@ -64,16 +64,16 @@ void UART_init(){
 
  /* Configure */
  UCA0CTL1 |= UCSSEL_2; // SMCLK
- UCA0BR0 = 0x41; // 8MHz 9600
- UCA0BR1 = 0x03; // 8MHz 9600
- UCA0MCTL = 2*UCBRS0; // Modulation UCBRSx = 2
+ UCA0BR0 = 104; // 1MHz 9600
+ UCA0BR1 = 0; // 1MHz 9600
+ UCA0MCTL = UCBRS0; // Modulation UCBRSx = 1
 
  /* Take UCA0 out of reset */
  UCA0CTL1 &= ~UCSWRST;
 
  IE2 |= UCA0RXIE; // Enable USCI_A0 RX interrupt
 
-//__bis_SR_register(GIE); // interrupts enabled
+__bis_SR_register(GIE); // interrupts enabled
 }
 
 /**
